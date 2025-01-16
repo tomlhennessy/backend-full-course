89d1f3657f5ee260ce603b540b265e65fbe38344
Tom Hennessy <tomlhennessy@outlook.com>
2025-01-16 10:51:01 +1000

    db tables

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -11,6 +11,16 @@ const __filename = fileURLToPath(import.meta.url);
 // get the directory name from the file path
 const __dirname = path.dirname(__filename);
 
+
+// Middleware
+app.use(express.json())
+// serves the HTML file from the public directory
+// tells express to serve all files from the public folder as static assets / files. Any requests for the CSS files will be resolved to the public directory.
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
