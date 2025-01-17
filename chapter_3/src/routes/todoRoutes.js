3d999e31e102ae6d1f4fe1d368a916336141c23a
Tom Hennessy <tomlhennessy@outlook.com>
2025-01-17 10:57:45 +1000

    auth middleware

--- chapter_3/src/routes/todoRoutes.js
+++ chapter_3/src/routes/todoRoutes.js
@@ -4,7 +4,10 @@ import db from '../db.js'
 const router = express.Router()
 
 // get all todos for logged in user
-router.get('/', (req, res) => {})
+router.get('/', (req, res) => {
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userID)
+})
 
 // create a new todo
 router.post('/', (req, res) => {})
